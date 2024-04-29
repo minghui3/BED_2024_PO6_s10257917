89713f0d7e6e470b42c517a4429bcced2a876bb5
minghui3 <s10257917@connect.np.edu.sg>
2024-04-29 12:54:47 +0800

    wasd

--- books-api/app.js
+++ books-api/app.js
@@ -10,16 +10,18 @@ let books = [
 app.use(express.json())
 // Configure body-parser to handle URL-encoded form data
 app.use(bodyParser.urlencoded({ extended: true })); // Set extended: true for nested objects
+
 app.get('/books', (req, res) => {
     res.json(books); // Send the array of books as JSON response
  });
 
- app.post('/books', (req, res) => {
-    const newBook = req.body; // Get the new book data from the request body
-    newBook.id = books.length + 1; // Assign a unique ID
-    books.push(newBook); // Add the new book to the array
-    res.status(201).json(newBook); // Send created book with status code 201
-   });
+app.post('/books', (req, res) => {
+const newBook = req.body; // Get the new book data from the request body
+newBook.id = books.length + 1; // Assign a unique ID
+books.push(newBook); // Add the new book to the array
+res.status(201).json(newBook); // Send created book with status code 201
+});
+
    app.get('/books/:id', (req, res) => {
     const bookId = parseInt(req.params.id); // Get book ID from URL parameter
     const book = books.find(book => book.id === bookId);
@@ -56,6 +58,7 @@ app.get('/books', (req, res) => {
           res.status(404).send('Book not found'); // Send error for non-existent book
         }
       });
-      app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-     });
+
+    app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    });
